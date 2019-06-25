1f8a0bc0f2509adb6a63636d08436b07f9c27990
Tom Klonikowski <tk.gih.1@kloni.net>
2019-06-25 15:48:45 +0200

    Add request view

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var config = require('./package').config;
 var express = require('express');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var app = express();
 
@@ -10,15 +11,33 @@ app.get('/', function (req, res) {
    res.send('OK');
 });
 
+var requestStore = [];
+
+// assign the mustache engine to .html files
+app.engine('html', cons.mustache);
+
+// set .html as the default extension 
+app.set('view engine', 'html');
+
+app.set('views', __dirname + '/views');
+
 app.post('/', function(req, res) {
   console.log(JSON.stringify(req.body));
-   if (req.body.payment && req.body.payment.statuses && req.body.payment.statuses.status) {
+  if (req.body.payment) {
+    if (req.body.payment.statuses && req.body.payment.statuses.status) {
       var status = req.body.payment.statuses.status;
       for (var i=0; i<status.length; i++) {
-         console.log(status[i].code + ": " + status[i].description);
+        console.log(status[i].code + ": " + status[i].description);
       }
-   }
+    }
+    requestStore.push(req);
+  }
   res.send('OK');
+});
+
+app.get('/', function(req, res) {
+  res.render('index', requestStore);
+  requestStore.splice(0, requestStore.length);
 });
 
 var server = app.listen(process.env.PORT || config.port, function () {
